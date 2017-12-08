6a2dd950590e73a82632f9dbec989d0c5c24e628
Domas <domas.bitvinskas@me.com>
2017-12-08 23:17:49 +0200

    Start connecting inputs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
+import { getFormValues } from 'redux-form';
+import { connect } from 'react-redux';
 
 import Chart from './components/Chart';
 import MainForm from './components/MainForm';
@@ -8,38 +10,19 @@ import MutualFundsReturn from './calculations/MutualFundsReturn';
 
 class App extends Component {
   ages() {
-    return _.range(this.inputs().currentAge, this.inputs().retirementAge + 1);
-  }
-
-  inputs() {
-    return {
-      currentAge: 30,
-      retirementAge: 65,
-      primaryCiAmount: 69000,
-      primaryIllness: true,
-      secondaryCiAmount: 92000,
-      secondaryIllness: true,
-      rateOfReturn: 0.05,
-      portfoliosFees: 0.005,
-      mutualFundsFees: 0.023,
-      includePrimaryCiInsurance: true,
-      primaryCiCost: 33,
-      includeSecondaryCiInsurance: true,
-      secondaryCiCost: 47,
-      pacMonth: 550,
-      initialInvestment: 75000,
-    };
+    return _.range(this.props.inputs.currentAge, this.props.inputs.retirementAge + 1);
   }
 
   result() {
     return _.map(this.ages(), (age) => ({
       age,
-      portfoliosReturn: _.round(new PortfoliosReturn(this.inputs()).calculate(age)),
-      mutualFundsReturn: _.round(new MutualFundsReturn(this.inputs()).calculate(age)),
+      portfoliosReturn: _.round(new PortfoliosReturn(this.props.inputs).calculate(age)),
+      mutualFundsReturn: _.round(new MutualFundsReturn(this.props.inputs).calculate(age)),
     }));
   }
 
   render() {
+    console.log(this.props);
     return (
       <div>
         <Chart data={this.result()} />
@@ -49,4 +32,6 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(state => ({
+  inputs: getFormValues('mainForm')(state) || {},
+}))(App);
--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import _ from 'lodash';
+
+const number = (value) => value && _.parseInt(value);
 
 class MainForm extends Component {
   render() {
@@ -9,7 +12,12 @@ class MainForm extends Component {
 
         <div>
           <label htmlFor='initialInvestment'>Initial Investment</label>
-          <Field name='initialInvestment' component='input' type='text' />
+          <Field
+            name='initialInvestment'
+            component='input'
+            type='text'
+            normalize={number}
+          />
         </div>
       </div>
     );
@@ -17,5 +25,22 @@ class MainForm extends Component {
 }
 
 export default reduxForm({
-  form: 'mainForm'
+  form: 'mainForm',
+  initialValues: {
+    currentAge: 30,
+    retirementAge: 65,
+    primaryCiAmount: 69000,
+    primaryIllness: true,
+    secondaryCiAmount: 92000,
+    secondaryIllness: true,
+    rateOfReturn: 0.05,
+    portfoliosFees: 0.005,
+    mutualFundsFees: 0.023,
+    includePrimaryCiInsurance: true,
+    primaryCiCost: 33,
+    includeSecondaryCiInsurance: true,
+    secondaryCiCost: 47,
+    pacMonth: 550,
+    initialInvestment: 75000,
+  },
 })(MainForm);
